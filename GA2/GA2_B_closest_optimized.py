c37f247f4bd6e6dbb8ed23395631284d585294df
youpie <youplamb@hotmail.nl>
2023-06-08 23:40:38 +0200

    made the plotting better

--- GA2/GA2_B_closest_optimized.py
+++ GA2/GA2_B_closest_optimized.py
@@ -11,11 +11,11 @@ environ['OMP_NUM_THREADS'] = '4'
 #calculate shortest path using Closest node heuristic
 
 start_point = 0                 #what point to start at
-attempts = 10                    #how often to run the program to get average execution time
-add_points = 0
+attempts = 10000                    #how often to run the program to get average execution time
+add_points = 0                     #how many points to add when the program is run
 add_point_every_loop = False
 
-def write_points(ammount,currlength,point_array):
+def write_points(ammount,currlength,point_array):           #fill the input file with more random points
     if(ammount != 0):
         with open('/home/youpie/OneDrive/School!/AMA4/Projecten/Groep project/AMA_2023_Group_assignment/GA2/inputGAS2B.csv', mode='w') as output_file:
             current_length = currlength
@@ -66,41 +66,32 @@ def write_csv(distance, route):                                     #write total
 
 
 
-def plot(points_objects,route):
-    temp_point_array = list()
+def plot(points_objects,route):             #plot the points
+    temp_point_array = list([[],[]])
     for i in range(len(points_objects)):
-        temp_point_array.extend([[]])
-        temp_point_array[i].append(points_objects[i].x)
-        temp_point_array[i].append(points_objects[i].y)
-    distances = np.linalg.norm(np.asarray(temp_point_array), axis=1) 
-
-    for i in range(len(distances)):
-        temp_point_array[i].append(distances[i])
-
-    temp_point_array = np.asarray(temp_point_array)
-    sorted_array = temp_point_array[temp_point_array[:, 2].argsort()]
-    plt.scatter(sorted_array[:,0],sorted_array[:,1])
-
-    for i in range(len(temp_point_array)):
-        plt.text(sorted_array[i,0],sorted_array[i,1],str(i))
-        plt.plot([points_objects[route[i]].x,points_objects[route[i+1]].x],[points_objects[route[i]].y,points_objects[route[i+1]].y])
-        
+        temp_point_array[0].append(points_objects[i].x)
+        temp_point_array[1].append(points_objects[i].y)
+    plt.scatter(temp_point_array[0],temp_point_array[1])
+    for i in range(len(points_objects)):
+        plt.text(temp_point_array[0][i],temp_point_array[1][i],str(i))
+        plt.plot([points_objects[route[i]].x,points_objects[route[i+1]].x],[points_objects[route[i]].y,points_objects[route[i]].y])         #create the 1x1 square movement requirements
+        plt.plot([points_objects[route[i+1]].x,points_objects[route[i+1]].x],[points_objects[route[i]].y,points_objects[route[i+1]].y])
     plt.show()
 
 def closest_point(points_array ,point):
     own_coords = np.array([points_array[point].x,points_array[point].y])
     temp_point_array = np.full((len(points),2),own_coords)
-    for i in range(len(points_array)):
+    for i in range(len(points_array)):                          #fill a 2D array with coordinates, instead of 2 arrays with x column and y column but just a bunch of xy arrays
         if points_array[i].distanceNearestPoint == 0:
             temp_point_array[i][0] = points_array[i].x
             temp_point_array[i][1] = points_array[i].y
     temp_point_array_norm = np.subtract(temp_point_array,own_coords)
-    distance_array = np.linalg.norm(temp_point_array_norm,axis=1)           #calculate distance to all points around point using pythagoran theorem
+    distance_array = np.linalg.norm(temp_point_array_norm,axis=1)           #use linear algebra to calculate the normalised vector values of the point from the origin
     try:
         closest_distance = np.min(distance_array[np.nonzero(distance_array)])
         closest_point = np.where(distance_array == closest_distance)
     except:
-        closest_distance = 0
+        closest_distance = 0                                                    #there is a very small chance that finding the minimal value goes wrong, this is to account for when that happens
         closest_point = [[0]]
     points[point].distanceNearestPoint = closest_distance
     points[point].nearestPoint = closest_point
@@ -131,8 +122,8 @@ for repeat in range(attempts):
         route.append(closest_next_point)
     else:
         route.append(start_point)
-        last_point = 0
-        for i,current_point in enumerate(route):
+        last_point = start_point
+        for i,current_point in enumerate(route):                                #calculate the total distance that has been traveled
             distance = inverse_pythagoran(points, last_point, current_point)
             last_point = current_point
             total_distance += distance[0]+distance[1]   
@@ -144,6 +135,6 @@ for repeat in range(attempts):
         points[i].distanceNearestPoint = 0
 write_csv(total_distance,route)
 np_execution_times = np.asarray(execution_times)
-print("average time: " + str(np.average(np_execution_times)))
+print("average time: " + str(np.average(np_execution_times)))                       #count exectution time
 print("total time for %s attempts: " %(attempts) + str(np.sum(np_execution_times)))
 plot(points,route)
