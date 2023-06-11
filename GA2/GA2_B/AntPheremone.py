f776040d9458917317125daf1ccfe6058b864cc2
Lilith207 <freekbertens26@gmail.com>
2023-06-11 21:28:02 +0200

    modified:   GA2/GA2_B/AlgoRun.py 	modified:   GA2/GA2_B/AntPheremone.py 	new file:   GA2/GA2_B/__pycache__/AntPheremone.cpython-311.pyc 	modified:   GA2/GA2_B/__pycache__/NearestNeigAlgo.cpython-311.pyc

--- GA2/GA2_B/AntPheremone.py
+++ GA2/GA2_B/AntPheremone.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
 import random
+import copy
 
 #imports 25 points and their x and y
 #exports route and total length
 
-class NearestNeighbour:
+class Pher:
     def __init__(self, Pointnr, X, Y):
         self.Pointnr = Pointnr
         self.PointX = X
@@ -25,22 +26,48 @@ class NearestNeighbour:
     
     def RunAlgo(self):
         DistanceArray = self.Distancearray()
-        ChanceArray = []
+        ChanceArraySav = []
         for i in range(len(self.PointX)):
             Column = []
             for j in range(len(self.PointY)):
-                Column.append(1)
-            ChanceArray.append(Column)
-        CurrentPoint = 0
-        TotalDistance = 0
-        Path = [[0],[0],[0]]
-        for i in range(len(self.PointX)-1):
-            Row = DistanceArray[CurrentPoint]
-            RowChance = ChanceArray[CurrentPoint]
-            sav = random.choices(Row, RowChance)
-            location = Row.index(sav)
-        Path[0].append(0)
-        Path[1].append(0)
-        Path[2].append(0)
+                if i == j:
+                    Column.append(0)
+                else:
+                    Column.append(1)
+            ChanceArraySav.append(Column)
+        for i in range(len(ChanceArraySav)):
+            ChanceArraySav[i][0] = 0
+
+        for i in range(1):
+            for i in range(100):#amount of ants to send out
+                CurrentPoint = 0
+                TotalDistance = 0
+                ChanceArray = copy.deepcopy(ChanceArraySav)
+                Path = [[0],[0],[0]]
+                for i in range(len(self.PointX)-1):
+                    Row = DistanceArray[CurrentPoint]
+                    RowSav = Row
+                    RowChance = ChanceArray[CurrentPoint]
+                    sav = random.choices(Row, RowChance)
+                    location = 0
+                    for i in range(len(RowSav)):
+                        if sav[0] == RowSav[i]:
+                            location = i
+                #location = np.where(RowSav)
+                    CurrentPoint = location
+                    for j in range(len(self.PointX)):
+                        ChanceArray[j][CurrentPoint] = 0
+                    TotalDistance += sav[0]
+                    Path[0].append(location)
+                    Path[1].append(self.PointX[location])
+                    Path[2].append(self.PointY[location])
+                EndCap = self.PointX[CurrentPoint] + self.PointY[CurrentPoint]
+                Addition = ((100/(EndCap+TotalDistance))*5)**2
+                Path[0].append(0)
+                Path[1].append(0)
+                Path[2].append(0)
+                for i in range(len(Path[0])-1): # use to increase pheremone count on good path
+                    ChanceArraySav[Path[0][i]][Path[0][i+1]] = ChanceArraySav[Path[0][i]][Path[0][i+1]]+Addition
+                    ChanceArraySav[Path[0][i+1]][Path[0][i]] = ChanceArraySav[Path[0][i+1]][Path[0][i]]+Addition
         TotalDistance += (self.PointX[CurrentPoint] + self.PointY[CurrentPoint])
         return Path, TotalDistance
