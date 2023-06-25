4d6437756d0dd060738ec31e714fd53bbb6d2a7d
Lilith207 <freekbertens26@gmail.com>
2023-06-25 19:39:19 +0200

    modified:   GA2/GA2_B/GA2_B.py 	modified:   GA2/GA2_B/PairwiseExchange.py 	new file:   GA2/GA2_B/RandobasedExchange.py 	new file:   GA2/GA2_B/__pycache__/PairwiseExchange.cpython-311.pyc 	new file:   GA2/GA2_B/__pycache__/RandobasedExchange.cpython-311.pyc 	deleted:    GA2/NearestNeigAlgo.py

--- GA2/GA2_B/GA2_B.py
+++ GA2/GA2_B/GA2_B.py
@@ -2,6 +2,7 @@ from NearestNeigAlgo import NearestNeighbour
 from PairwiseExchange import Exhange
 from AntPheremone import Pher
 from RandoTry import RandomTries
+from RandobasedExchange import RandomExchange 
 import random
 from RandomPoints import RandomPoints
 from matplotlib import pyplot
@@ -12,7 +13,7 @@ def TestAlgo(Algo):
     Ftime = time.time() #start checking time
     TotalDistance = 0
     for i in range(100):#for the average run amount of distance
-        NR, Xs, Ys = RandomPoints(30) #calls up a function in the randompoints file to generate 90 unique points
+        NR, Xs, Ys = RandomPoints(90) #calls up a function in the randompoints file to generate 90 unique points
         NBAlgo = Algo(NR, Xs, Ys) #runs the algorithm with the generated points
         Path, Distance = NBAlgo.RunAlgo()
         TotalDistance += Distance
@@ -23,7 +24,7 @@ def TestAlgo(Algo):
     pyplot.show()
     return TotalDistance/100, TotalTime
 
-Distance, Time = TestAlgo(NearestNeighbour)#current algorithms are Pher, NearestNeighbour and RandomTries, they are the import files
+Distance, Time = TestAlgo(Exhange)#current algorithms are Pher, NearestNeighbour and RandomTries, they are the import files
 #this will give you the average distance the algorithm gives and how long it takes to do so
 #this allows you to test algorithms by importing them as classes and running them a lot with random points
 print(Distance)
--- GA2/GA2_B/RandobasedExchange.py
+++ GA2/GA2_B/RandobasedExchange.py
@@ -1,27 +1,20 @@
 import numpy as np
 import math
+import random
+import copy
 
 #imports 25 points and their x and y
 #exports route and total length
 
-class Exhange:
-    def __init__(self, Pointnr, X, Y):#adds all points as input
-        self.Pointnr = Pointnr
+class RandomExchange:#this algorithm is very simple, generate a random path, how long is this path?
+    def __init__(self, Pointnr, X, Y):#is it shorter than the shortest found path? yes then save the path and the distance
+        self.Pointnr = Pointnr #and do this for x amount of times
         self.PointX = X
         self.PointY = Y
         self.PointX.insert(0,0)
         self.PointY.insert(0,0)
         self.Pointnr.insert(0,0)
 
-    def Distancearray(self):#calculates a distance array list prim and kruskal
-        Array = []
-        for i in range(len(self.PointX)):
-            Column = []
-            for j in range(len(self.PointY)):
-                Column.append(abs(self.PointX[i]-self.PointX[j])+abs(self.PointY[i]-self.PointY[j]))
-            Array.append(Column)
-        return Array
-    
     def Switch(self, A, B, Path):
         Inbet0 = Path[0][A+1]
         Path[0][A+1] = Path[0][B]
@@ -64,31 +57,33 @@ class Exhange:
                     Totaldistance = (Totaldistance - CurrentLength) + SwitchLength
         return Path, Totaldistance
     
-    def RunAlgo(self):#this algorithm is a lot alike prim, except it finds the smallest connection on the current point
-        DistanceArray = self.Distancearray()
-        CurrentPoint = 0
-        TotalDistance = 0
+    def RunAlgo(self):
+        MinDistance = 999999999
+        PathSav = []
         Path = [[0],[0],[0]]
-        for i in range(len(self.PointX)-1):
-            Row = DistanceArray[CurrentPoint]
-            sav = 99999999
-            for Num in Row:
-                if Num < sav and Num != 0:
-                    sav = Num
-            location = Row.index(sav)           
-            for j in range(len(self.PointX)):
-                DistanceArray[j][CurrentPoint] = 0
-            CurrentPoint = location
-            TotalDistance += sav
-            Path[0].append(location)
-            Path[1].append(self.PointX[location])
-            Path[2].append(self.PointY[location])
-        Path[0].append(0)
-        Path[1].append(0)
-        Path[2].append(0)
-        TotalDistance += (self.PointX[CurrentPoint] + self.PointY[CurrentPoint])
-        Path, TotalDistance = self.PairExchange(Path, TotalDistance)
-        Path, TotalDistance = self.PairExchange(Path, TotalDistance)
-        Path, TotalDistance = self.PairExchange(Path, TotalDistance)
-        return Path, TotalDistance
-    
+        for i in range(1):
+            Points = copy.deepcopy(self.Pointnr)
+            RandoPath=[]
+            TotalDistance = 0
+            for j in range(len(self.Pointnr)):
+                NewPoint = random.choice(Points)
+                RandoPath.append(NewPoint)
+                Points.pop(Points.index(NewPoint))
+            RandoPath.append(0)
+            for j in range(len(RandoPath)-1):
+                Current = RandoPath[j]
+                Next = RandoPath[j+1]
+                TotalDistance += (abs(self.PointX[Current]-self.PointX[Next])+abs(self.PointY[Current]-self.PointY[Next]))
+            if TotalDistance < MinDistance:
+                MinDistance = TotalDistance
+                PathSav = RandoPath
+        PathSav.pop(0)
+        for Point in PathSav:
+            Path[0].append(self.Pointnr[Point])
+            Path[1].append(self.PointX[Point])
+            Path[2].append(self.PointY[Point])
+        Path, MinDistance = self.PairExchange(Path, MinDistance)
+        Path, MinDistance = self.PairExchange(Path, MinDistance)
+        Path, MinDistance = self.PairExchange(Path, MinDistance)
+        Path, MinDistance = self.PairExchange(Path, MinDistance)
+        return Path, MinDistance
